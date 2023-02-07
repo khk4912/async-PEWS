ad9efada5a28631c64ffd09e4ff5c5f2b4e8904b
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-02-07 17:23:20 +0900

    🩹 chore: Set imports in __init__.py

--- async_pews/__init__.py
+++ async_pews/__init__.py
@@ -1,2 +1,6 @@
-from .client.client import *
-from .model.model import *
+from .client.pews import PEWS
+
+from .sim.sim import SimulationPEWS
+from .sim.data import DefaultSimulationDataset
+
+from .model.model import EarthquakeEvent, Station
