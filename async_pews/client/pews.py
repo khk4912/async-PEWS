2fae637ee0b7c7e9d372ebe2e7c4b8cbc9acd0af
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-02-07 17:33:58 +0900

    ✨ feat: Introduce logger

--- async_pews/client/pews.py
+++ async_pews/client/pews.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 
 from ..model.model import EarthquakeEvent
@@ -7,7 +8,10 @@ from .client import HTTPClient
 class PEWS:
     def __init__(self) -> None:
         self.__latest_eqk_time = 0
-        self.phase: int = 1
+        self.__setup_logger()
+
+    def __setup_logger(self) -> None:
+        self.__logger = logging.getLogger("async_pews")
 
     async def start(self) -> None:
         PEWSClient = HTTPClient()
@@ -21,6 +25,8 @@ class PEWS:
 
             self.phase = PEWSClient._phase
             self._phase_handler(PEWSClient, self.phase)
+
+            self.__logger.debug("Event on_loop")
             asyncio.create_task(self.on_loop())
 
     def _phase_handler(self, PEWSClient: HTTPClient, phase: int) -> None:
@@ -28,6 +34,7 @@ class PEWS:
         match phase:
 
             case 1:
+                self.__logger.debug("Event on_phase_1")
                 asyncio.create_task(self.on_phase_1())
 
             case 2 | 3:
@@ -37,11 +44,13 @@ class PEWS:
                 ):
                     match PEWSClient._phase:
                         case 2:
+                            self.__logger.debug("Event on_new_early_warning")
                             asyncio.create_task(
                                 self.on_new_early_warning(PEWSClient._eqk_event)
                             )
                             asyncio.create_task(self.on_phase_2(PEWSClient._eqk_event))
                         case 3:
+                            self.__logger.debug("Event on_new_eqk_info")
                             asyncio.create_task(
                                 self.on_new_earthquake_info(PEWSClient._eqk_event)
                             )
@@ -52,39 +61,34 @@ class PEWS:
                 elif PEWSClient._eqk_event:
                     match phase:
                         case 2:
+                            self.__logger.debug("Event on_phase_2")
                             asyncio.create_task(self.on_phase_2(PEWSClient._eqk_event))
                         case 3:
+                            self.__logger.debug("Event on_phase_3")
                             asyncio.create_task(self.on_phase_3(PEWSClient._eqk_event))
                         case 4:
+                            self.__logger.debug("Event on_phase_4")
                             asyncio.create_task(self.on_phase_4())
 
     async def on_new_early_warning(self, eqk_event: EarthquakeEvent):
-        print("New Early Warning")
-        print(eqk_event)
         ...
 
     async def on_new_earthquake_info(self, eqk_event: EarthquakeEvent):
-        print("New Earthquake Info")
-        print(eqk_event)
-
         ...
 
     async def on_phase_1(self):
-        pass
+        ...
 
     async def on_loop(self):
-        pass
+        ...
 
     async def on_phase_2(self, eqk_event: EarthquakeEvent):
-        print("Phase 2")
         ...
 
     async def on_phase_3(self, eqk_event: EarthquakeEvent):
-        print("Phase 3")
         ...
 
     async def on_phase_4(self):
-        print("Phase 4")
         ...
 
     def run(self) -> None:
