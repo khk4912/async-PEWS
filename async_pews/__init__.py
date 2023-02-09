8a24c7c7d3183f04f3a2d7320d02c800677338e5
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-02-09 15:28:11 +0900

    🐛 fix: Add EarlyWarningInfo in model init

--- async_pews/__init__.py
+++ async_pews/__init__.py
@@ -3,4 +3,4 @@ from .client.pews import PEWSClient
 from .sim.sim import SimulationPEWS
 from .sim.data import SimulationDataset
 
-from .model.model import EarthquakeEvent, Station
+from .model.model import EarthquakeEvent, EarlyWarningInfo
